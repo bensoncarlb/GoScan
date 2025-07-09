7c30fe2cf48ea2df09dd4911d35705407c2c70b2
Carl Benson <github@bensn.org>
2025-07-08 18:29:55 -0700

    Starting OCR package, thread data server processing

---
+++ internal/ocr/main.go
@@ -0,0 +1,6 @@
+package ocr
+
+func ReadImage(i *[]byte) (string, error) {
+
+	return "test", nil
+}
--- internal/processing/main.go
+++ internal/processing/main.go
@@ -13,7 +13,42 @@ import (
 	"github.com/bensoncb/GoScan/internal/structs"
 )
 
-func data(w http.ResponseWriter, req *http.Request) {
+type data_rcvd struct {
+	ch chan structs.InputFile
+}
+
+func process(ch <-chan structs.InputFile) {
+	for {
+		//Block waiting for new item to process
+		d := <-ch
+
+		log.Println("Process routine received new item for processing: ", d.Name)
+
+		//Save off received data
+		f, err := os.Create("rcvd/" + d.Name)
+
+		if err != nil {
+			panic(err)
+		}
+
+		f.Write(d.Data)
+		f.Close()
+
+		//"Read" the incoming item for indexing data
+		s, err := ocr.ReadImage(&d.Data)
+
+		if err != nil {
+			panic(err)
+		}
+
+		//Print off results
+		println(s)
+
+		fmt.Println(d)
+	}
+}
+
+func (dr *data_rcvd) data(w http.ResponseWriter, req *http.Request) {
 	log.Println("Received new request", req.RemoteAddr)
 
 	d := &structs.InputFile{}
@@ -23,29 +58,15 @@ func data(w http.ResponseWriter, req *http.Request) {
 		panic(err)
 	}
 
-	fmt.Printf("%+v\n", d)
 	w.WriteHeader(http.StatusCreated)
 
-	f, err := os.Create("rcvd/" + d.Name)
-
-	if err != nil {
-		panic(err)
-	}
-
-	defer f.Close()
-
-	f.Write(d.Data)
-
-	s, err := ocr.ReadImage(&d.Data)
-
-	if err != nil {
-		panic(err)
-	}
-
-	println(s)
+	dr.ch <- *d
 }
 
 func main() {
+	log.Println("Data Server Starting up")
+
+	//Check the directory to (for now) store received data in exists
 	_, err := os.Stat("rcvd")
 
 	if errors.Is(err, os.ErrNotExist) {
@@ -56,9 +77,16 @@ func main() {
 		}
 	}
 
-	http.HandleFunc("/data", data)
+	//Setup a channel for processing incoming input files
+	ch_process := &data_rcvd{ch: make(chan structs.InputFile)}
 
-	log.Println("Data Server Starting up")
+	for range 1 {
+		//Kick off routine(s) to listen for new items to process
+		go process(ch_process.ch)
+	}
+
+	//Setup HTTP listener and handler
+	http.HandleFunc("/data", ch_process.data)
 
 	go http.ListenAndServe(":8090", nil)
 
