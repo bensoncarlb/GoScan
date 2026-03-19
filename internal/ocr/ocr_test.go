ba496a2944abd0d0ab96cd249154d455ff4aa1a7
Carl Benson <github@bensn.org>
2026-03-19 16:15:59 -0700

    Base test case for ocr.go

---
+++ internal/ocr/ocr_test.go
@@ -0,0 +1,34 @@
+package ocr_test
+
+import (
+	"testing"
+
+	"github.com/bensoncb/GoScan/internal/ocr"
+	"github.com/bensoncb/GoScan/internal/structs/inputFile"
+)
+
+/*
+* Check a basic valid case
+ */
+func TestValid(t *testing.T) {
+	iFile, err := inputFile.New(1, "test", []byte("test"))
+
+	if err != nil {
+		t.Fatalf("Failed to setup inputFile: %s", err)
+	}
+
+	err = ocr.FormIdentify(&iFile)
+
+	if err != nil {
+		t.Fatalf("Form Identify failed: %s", err)
+	}
+
+	res, err := ocr.ReadImage(&iFile.Data)
+
+	if err != nil {
+		t.Fatalf("Error during data read: %s", err)
+	} else if res != "test" {
+		t.Fatalf("Form data read failed. Expected: %s, Got: %s", "test", res)
+	}
+
+}
