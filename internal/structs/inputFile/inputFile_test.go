4e5812a35d9fd2bb5af6bbb944e55a031814d522
Carl Benson <github@bensn.org>
2026-03-18 18:15:00 -0700

    Move inputFile test cases to standalone package.

--- internal/structs/inputFile/inputFile_test.go
+++ internal/structs/inputFile/inputFile_test.go
@@ -1,14 +1,16 @@
-package inputFile
+package inputFile_test
 
 import (
 	"errors"
 	"testing"
+
+	"github.com/bensoncb/GoScan/internal/structs/inputFile"
 )
 
 func TestBaseGood(t *testing.T) {
 	data := []byte("test")
 
-	_, err := New(1, "test", data)
+	_, err := inputFile.New(1, "test", data)
 
 	if err != nil {
 		t.Fatalf("Test failed with error: %s", err)
@@ -18,11 +20,11 @@ func TestBaseGood(t *testing.T) {
 func TestBadArgs(t *testing.T) {
 	data := []byte("test")
 	name := "test"
-	iFile, err := New(0, name, data)
+	iFile, err := inputFile.New(0, name, data)
 
 	if err == nil {
 		t.Fatalf("Missing Size not detected.")
-	} else if !errors.Is(err, ErrBadParam{}) {
+	} else if !errors.Is(err, inputFile.ErrBadParam{}) {
 		t.Fatalf("Unexpected error returned: %s", err)
 	} else if iFile.Name != name {
 		t.Fatalf("Setting Name property failed. Acutal: %s, Expected: %s", iFile.Name, name)
